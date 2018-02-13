ebe694fe3d43d8ea43feda587a1066f7947d0a8b
jsrj <rj.javadev@icloud.com>
2018-02-13 15:52:23 -0500

    included ojdbc7 and begun building out connection statements. Testing of statement is pending installation of an Oracle SQL db.

--- src/App.java
+++ src/App.java
@@ -2,20 +2,25 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import oracle.jdbc.OracleDriver;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.sql.*;
 import javax.net.ssl.HttpsURLConnection;
 
 public class App {
+
+    OracleDriver driver = new OracleDriver();
 
     public static void main(String[] args) {
         App http = new App();
 
         System.out.println(
                 " ----------------------- \n" +
-                        "| Opening Connection... |\n" +
-                        " ----------------------- \n"
+                "| Opening Connection... |\n" +
+                " ----------------------- \n"
         );
         try {
             http.sendGet();
@@ -27,21 +32,56 @@ public class App {
     // HTTP GET request - Read a specific file from within a repository.
     private void sendGet() throws Exception {
 
+        Statement  statement;
+        Connection cnx;
+        // Create a connection to the database
+        try {
+            String dbDriver       = "oracle.jdbc.OracleDriver";
+            String serverName     = "127.0.0.1";
+            String portNumber     = "1521";
+            String sid            = "mydatabase";
+            String url            = "jdbc:oracle:thin:@" + serverName + ":" + portNumber + ":" + sid;
+            String username       = "username";
+            String password       = "password";
+
+            System.out.println(url);
+
+            Class.forName(dbDriver);
+
+            cnx       = DriverManager.getConnection(url, username, password);
+            statement = cnx.createStatement();
+
+            // Get all the things from the DB
+            ResultSet allTheThings = statement.executeQuery("SELECT * FROM things;");
+            System.out.println(allTheThings);
+        }
+        catch (ClassNotFoundException e) {
+            // Could not find the database driver
+            System.out.println("Database driver not found.");
+        }
+        catch (SQLException e) {
+            // Could not connect to the database
+            System.out.println("Could not connect to the database with information provided.");
+        }
+
+
+
+
+
+        // Establish Github Connection
         String username     = "jsrj";
         String filename     = "/dl1-c/dl2-c1/testfile7.txt";
         String reponame     = "test-repo";
 
         String downloadPath = "https://raw.githubusercontent.com/"+username+"/"+reponame+"/master"+filename;
         String repoContents = "https://api.github.com/repos/"     +username+"/"+reponame+"/contents";
-        String uri          = repoContents;
+        String uri          = downloadPath;
 
         URL obj = new URL(uri);
         HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
 
-        // optional default is GET
         con.setRequestMethod("GET");
 
-        //add request header
         con.setRequestProperty("User-Agent"    , "GH-Extractor/1.0");
         con.setRequestProperty("Accept"        , "application/vnd.github.v3+json");
         con.setRequestProperty("Username"      ,  username);
@@ -95,6 +135,7 @@ public class App {
 
                 break;
         }
+
 
 
     }
